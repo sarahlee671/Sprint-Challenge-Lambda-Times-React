d978a69d839a0d3bf10d8043362ee1e839d29b07
Sarahlee671 <sarah.lee1987@hotmail.com>
2019-05-03 09:32:00 -0700

    Added PropTypes to Tab component

--- lambdatimes/src/components/Content/Tab.js
+++ lambdatimes/src/components/Content/Tab.js
@@ -6,7 +6,7 @@ const Tab = props => {
       if it is not it should just be 'tab'*/
   return (
     <div
-      className={`tab ${props.tab.selectedTab ? " selectedTab" : ""}`}
+      className={`tab ${props.tab.selectedTab ? " tab active-tab" : "tab"}`}
       onClick={() => {props.selectedTab(props.tab)
         /* Replace this dummy click handler function with your selectTabHandler function from props 
          you'll need to pass the `tab` in as an argument to this handler. */
